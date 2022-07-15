c3d864a01d8d50accf7763ab051084956050f7c0
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-07-15 01:49:28 -0700

    add google fonts
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type {LinksFunction, MetaFunction} from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,6 +7,26 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+import styles from "~/styles/app.css"
+
+
+export const links: LinksFunction = () => ([
+  { rel: "stylesheet", href: styles },
+  {
+    rel: "preconnect",
+    href: "https://fonts.googleapis.com",
+  },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&family=Frank+Ruhl+Libre:wght@300;400;500;700;900&display=swap"
+  }
+])
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
