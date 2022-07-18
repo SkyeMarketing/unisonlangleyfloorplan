2c275444891a1f274c21ee9ab5effac6c934b53b
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-07-17 19:04:27 -0700

    units field is only needed for unit selection page
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- app/types/PlanData$Client.ts
+++ app/types/PlanData$Client.ts
@@ -1,6 +1,6 @@
 import type PlanData from "~/types/PlanData";
 
-export interface PlanData$Layout extends Omit<PlanData, "category" | "unavailable"> {}
+export interface PlanData$Layout extends Omit<PlanData, "category" | "unavailable" | "units"> {}
 
 type PlanData$Client = {
   [key: string]: [PlanData$Layout]
