7c8c50d552eae702bf557f0330a8b2596581c189
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-07-29 20:34:13 -0700

    remove previous plans
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- app/data/plans/Plans.server.tsx
+++ app/data/plans/Plans.server.tsx
@@ -1,36 +1,7 @@
 import type PlanData from "~/types/PlanData";
-import A1 from "~/data/plans/A1.server";
-import A2 from "~/data/plans/A2.server";
-import A2a from "~/data/plans/A2a.server";
-import A3 from "~/data/plans/A3.server";
-import B1 from "~/data/plans/B1.server";
-import C1 from "~/data/plans/C1.server";
-import B4 from "~/data/plans/B4.server";
-import B3 from "~/data/plans/B3.server";
-import B2 from "~/data/plans/B2.server";
-import A6 from "~/data/plans/A6.server";
-import A5 from "~/data/plans/A5.server";
-import A4 from "~/data/plans/A4.server";
-import C2 from "~/data/plans/C2.server";
-import C3 from "~/data/plans/C3.server";
-import A from "~/data/plans/A.server";
 
 const Plans: PlanData[] = [
-  A1,
-  A2,
-  A2a,
-  A3,
-  A4,
-  A5,
-  A6,
-  B1,
-  B2,
-  B3,
-  B4,
-  C1,
-  C2,
-  C3,
-  A,
+
 ]
 
 export default Plans
