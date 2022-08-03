e649858e8b8e2d81be56e4d8f1742c15d22f4a8d
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-08-02 21:49:43 -0700

    try making the buttons bigger
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- app/components/UnitButton.tsx
+++ app/components/UnitButton.tsx
@@ -10,13 +10,14 @@ const UnitButton: React.FC<UnitButtonProps> = ({number}): JSX.Element => {
       className={`
         group
         hover:bg-aqua
-        py-6
+        py-6 lg:py-16
         block
         border-2
+        border-black
         peer-hover:bg-aqua
         peer-checked:border-aqua
         rounded-lg
-        w-64 lg:w-80
+        w-64 lg:w-96
         overflow-hidden
         hover:border-black
       `}
@@ -24,7 +25,7 @@ const UnitButton: React.FC<UnitButtonProps> = ({number}): JSX.Element => {
       value={number}
       type={"submit"}
     >
-      <div className="text-black group-hover:text-white font-serif">
+      <div className="text-black group-hover:text-white font-serif text-2xl">
         <p className="text-center">{number}</p>
       </div>
     </button>
