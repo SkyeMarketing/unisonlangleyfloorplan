0a67c62f10e69c194711ea9c290faace7323a255
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-09-05 20:30:29 -0700

    more polish
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- app/routes/thank-you.tsx
+++ app/routes/thank-you.tsx
@@ -13,8 +13,8 @@ export const links: LinksFunction = () => [
 const  ThankYou: React.FC = (): JSX.Element => {
   return (
     <>
-      <main className={`w-screen h-screen bg-img bg-cover bg-no-repeat bg-blend-darken py-16`}>
-        <p className={`block w-[30vw] mx-auto bg-black/50 text-center  font-serifCaps text-white text-3xl px-24 py-16 rounded-2xl`}>Thank you for registering</p>
+      <main className={`w-screen h-screen bg-img bg-cover bg-no-repeat bg-blend-darken py-16 `}>
+        <p className={`block w-[30vw] mx-auto bg-black/50 text-center bg-blend-hard-light font-serifCaps text-white text-3xl px-24 py-16 rounded-2xl backdrop-blur-lg shadow-md shadow-aqua/50`}>Thank you for registering</p>
       </main>
     </>
   )
