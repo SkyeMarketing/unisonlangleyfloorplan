a9888d81d6eec7e144301a20901e91682b2e1582
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-09-09 18:34:05 -0700

    try iframing in an iframe?
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- app/routes/thank-you.tsx
+++ app/routes/thank-you.tsx
@@ -13,11 +13,7 @@ export const links: LinksFunction = () => [
 const  ThankYou: React.FC = (): JSX.Element => {
   return (
     <>
-      <main className={`w-screen h-screen bg-img bg-cover bg-no-repeat bg-blend-darken`}>
-        <div className={`w-full h-full bg-aqua/10 bg-blend-hard-light pt-16`}>
-          <p className={`block w-[80vw] md:w-[50vw] lg:w-[30vw] mx-auto bg-black/50 text-center bg-blend-hard-light font-serifCaps text-white text-3xl px-24 py-16 rounded-2xl backdrop-blur-lg shadow-md shadow-aqua/50`}>Thank you for registering</p>
-        </div>
-      </main>
+      <iframe src={`https://www.unisonlangley.ca/thank-you`} className={`border-0 w-screen `} />
     </>
   )
 }
