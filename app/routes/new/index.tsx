1ff75a4bec7e987dd2ef0e4b871f2d7d1948ef77
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-09-15 14:32:09 -0700

    working layout for the scrollable bar
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- app/routes/new/index.tsx
+++ app/routes/new/index.tsx
@@ -1,4 +1,4 @@
-import type {ActionFunction, LoaderFunction} from "@remix-run/node";
+import type {ActionFunction, LinksFunction, LoaderFunction} from "@remix-run/node";
 import {json, redirect} from "@remix-run/node";
 import {Form, useLoaderData} from "@remix-run/react";
 import React from "react";
@@ -35,93 +35,6 @@ export const loader: LoaderFunction = () => {
   return json(data)
 }
 
-// const Route: React.FC = (): JSX.Element => {
-//   const data: LoaderData = useLoaderData()
-//
-//   return (
-//
-//     <div
-//       className={`
-//           container
-//           bg-white
-//           mx-auto
-//           px-8
-//           py-16
-//           flex
-//           justify-center
-//         `}
-//     >
-//       <Form
-//         className={`
-//             flex
-//             flex-col
-//             flex-wrap
-//             gap-y-4
-//             overflow-x-hidden
-//           `}
-//         method={"post"}
-//       >
-//         {
-//           Object
-//             .entries(data)
-//             .map(([category, plans]) => {
-//               return (
-//                 <fieldset
-//                   className={`
-//                       mt-16
-//                       mx-2
-//                       px-2
-//                       py-8
-//
-//                     `}
-//                   key={category}
-//                 >
-//                   <legend
-//                     className={`
-//                         font-serif
-//                         text-7xl
-//                         font-bold
-//                         text-center
-//                         text-aqua
-//                       `}
-//                   >
-//                     {category}
-//
-//                   </legend>
-//
-//                   <div
-//
-//                   >
-//
-//                   <div
-//                     className={`
-//                         flex
-//                         w-128
-//                         overflow-x-scroll
-//                         mx-auto
-//                         my-8
-//                       `}
-//                   >
-//                     {
-//                       plans
-//                         .map(({area, layout, plan}) => {
-//                           return (
-//                             <PlanButton key={plan} value={plan} className={`mx-4`}>
-//                               <PlanCard key={plan} area={area} layout={layout} plan={plan}/>
-//                             </PlanButton>
-//                           )
-//                         })
-//                     }
-//                   </div>
-//                   </div>
-//                 </fieldset>
-//               )
-//             })
-//         }
-//       </Form>
-//     </div>
-//   );
-// }
 const Route: React.FC = (): JSX.Element => {
   const data: LoaderData = useLoaderData()
 
@@ -129,6 +42,8 @@ const Route: React.FC = (): JSX.Element => {
     <div
       className={`
         container
+        border-x
+        border-aqua/25
         bg-white
         mx-auto
         px-8
@@ -148,9 +63,9 @@ const Route: React.FC = (): JSX.Element => {
       >
         {
           Object.entries(data)
-            .map(([cat, plans]) => {
+            .map(([cat, plans], categoryIdx) => {
               const category: Category = cat as Category;
-
+              console.log(categoryIdx)
               return (
                 <fieldset
                   className={`
@@ -176,28 +91,23 @@ const Route: React.FC = (): JSX.Element => {
 
                   <div
                     className={`
-                      border-2 
-                      border-red-500
-                      overflow-x-scroll
+                      flex
+                      ${categoryIdx !== 0 ? 'lg:justify-center': ''}
+                      mx-auto
+                      overflow-x-auto
                       w-screen md:container
+                      gap-x-8
                     `}
                   >
-                    <div
-                      className={`
-                        flex
-                        
-                      `}
-                    >
-                      {
-                        plans.map(({area, layout, plan}) => {
-                          return (
-                            <PlanButton key={plan} value={plan} className={`mx-4`}>
+                    {
+                      plans.map(({area, layout, plan}, index) => {
+                        return (
+                            <PlanButton key={plan} value={plan} className={`snap-center snap-always scroll-ml-96`}>
                               <PlanCard area={area} layout={layout} plan={plan}/>
                             </PlanButton>
-                          )
-                        })
-                      }
-                    </div>
+                        )
+                      })
+                    }
                   </div>
                 </fieldset>
               )
