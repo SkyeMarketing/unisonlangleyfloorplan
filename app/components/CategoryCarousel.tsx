9b513953c583fc1e3109ef7eaf64f16c4d4d2ded
Hunar Roop Kahlon <hunar.roop@gmail.com>
2022-09-22 15:40:28 -0700

    more work
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- app/components/CategoryCarousel.tsx
+++ app/components/CategoryCarousel.tsx
@@ -1,8 +1,9 @@
 import {z} from "zod";
 import AreaSchema from "~/schemas/Area.schema";
-import IdSchema from "~/schemas/Id.schema";
 import NameSchema from "~/schemas/Name.schema";
+import IdSchema from "~/schemas/Id.schema";
 import LayoutSchema from "~/schemas/Layout.schema";
+import {useCallback, useEffect, useRef, useState} from "react";
 
 const CategoryCarouselPropsSchema = z.object({
   plans: z.array(z.object({
@@ -12,40 +13,48 @@ const CategoryCarouselPropsSchema = z.object({
     name: NameSchema,
   }))
 })
-type CategoryCarouselProps = z.infer<typeof CategoryCarouselPropsSchema>;
 
-export default ({plans}: CategoryCarouselProps) => (
-  <>
-    {
-      plans
-        .map((
-          {
-            area,
-            id,
-            layout,
-            name
-          }) => {
-          return (
-            <li
-              key={id}
-              className={`
-                flex
-                flex-col
-                gap-y-2
-                flex-1
-                p-2
-                border
-                border-aqua
-                border-opacity-50
-                rounded
-                bg-white
-              `}
-            >
-              <p>{name}</p>
-              <p>{area}</p>
-              <p>{layout}</p>
-            </li>
-          )
-        })}
-  </>
-)
+const CategoryCarousel = ({plans}: z.infer<typeof CategoryCarouselPropsSchema>) => {
+  const [current, setCurrent] = useState(0);
+
+  const leftRef = useRef<HTMLButtonElement>(null);
+  const rightRef = useRef<HTMLButtonElement>(null);
+  
+  useEffect(() => {
+    
+    if (leftRef.current) {
+      const left = leftRef.current;
+      
+      left.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        if (current > 0) {
+          setCurrent(current - 1);
+        }
+      })
+    }
+    
+    if(rightRef.current) {
+      const right = rightRef.current;
+
+      right.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        if (current < plans.length - 1) {
+          setCurrent(current + 1);
+        }
+      });
+    }
+  }, [current, plans.length])
+
+  return (
+    <div>
+      
+        <button type={`button`} ref={leftRef}>Left</button>
+        {plans[current].id}
+        <button type={"button"} ref={rightRef}>Right</button>
+      
+    </div>
+  )
+}
+export default CategoryCarousel
